b974a29772d72a028294eab0b37c189c7d159bfd
voonyaka <voonyaka@gmail.com>
2023-12-17 21:14:56 +0100

    last ver

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import shutil
 import zipfile
 import sys
 
-
 def normalize(name):
     name = name.lower()
     translit_table = str.maketrans("абвгдеёзийклмнопрстуфхъыэ", "abvgdeezijklmnoprstufh'ye")
@@ -11,28 +10,24 @@ def normalize(name):
     name = ''.join([char if char.isalnum() or char in ['.', '_'] else '_' for char in name])
     return name
 
-
 def process_folder(folder_path):
     items = list(folder_path.iterdir())
 
     for item in items:
         if item.is_file():
             process_file(item, folder_path)
-
-    # Перевірка чи папка порожня
+    
     if not any(folder_path.iterdir()):
         folder_path.rmdir()
         return
-
-    # Рекурсивно викликаємо функцію для всіх вкладених папок
+    
     for item in items:
         if item.is_dir() and item.name.lower() not in ['archives', 'videos', 'audio', 'documents', 'images', 'others']:
             process_folder(item)
 
-        # Видалення порожніх вкладених папок
+        
         if item.is_dir() and not any(item.iterdir()):
             item.rmdir()
-
 
 def process_file(file_path, target_path):
     file_extension = file_path.suffix.lower()[1:]
@@ -51,54 +46,41 @@ def process_file(file_path, target_path):
         extract_folder = target_path / 'archives' / normalized_name
 
         try:
-            # Створити папку для розпакованих файлів
+            
             extract_folder.mkdir(parents=True, exist_ok=True)
-
-            # Розпакувати архів
+            
             if file_extension == 'zip':
                 with zipfile.ZipFile(file_path, 'r') as zip_ref:
                     zip_ref.extractall(extract_folder)
-
-            # Видалити архів
+            
             file_path.unlink()
-
-            # Перемістити розпаковані файли
+            
             for extracted_item in extract_folder.iterdir():
                 destination_path = target_path / destination_folder / extracted_item.name
                 extracted_item.rename(destination_path)
-
-            # Видалити порожню папку розпакованих файлів
+            
             extract_folder.rmdir()
-
         except Exception as e:
             print(f"Error extracting {file_path.name}: {e}")
-
         return
     else:
         destination_folder = 'others'
-
-    # Створити папку призначення, якщо вона не існує
+    
     (target_path / destination_folder).mkdir(parents=True, exist_ok=True)
 
     destination_path = target_path / destination_folder / file_path.name
     shutil.move(file_path, destination_path)
-
-
 def main():
-    # Отримати аргумент командного рядка для шляху до папки
+    
     if len(sys.argv) != 2:
-        print("Usage: python <script.py> <folder_path>")
+        print("Usage: python <main.py> <folder_path>")
         sys.exit(1)
-
     folder_path = sys.argv[1]
     target_path = Path(folder_path)
 
     for folder in ['images', 'videos', 'documents', 'audio', 'archives', 'others']:
         (target_path / folder).mkdir(parents=True, exist_ok=True)
-
     process_folder(target_path)
 
-
-# Запуск основної функції зазначеної папки
 main()
 
